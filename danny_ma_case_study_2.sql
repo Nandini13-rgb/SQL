e06c96749274dbe867e441bf4588223f6b507818
Nandini13-rgb <65849848+Nandini13-rgb@users.noreply.github.com>
2023-05-25 19:38:03 +0530

    Update danny_ma_case_study_2.sql

--- danny_ma_case_study_2.sql
+++ danny_ma_case_study_2.sql
@@ -51,3 +51,28 @@ c.order_id = o.order_id
 group by o.customer_id
 ;
 
+
+-- Question 6 Query:
+with cte as(
+  select order_id,
+  runner_id,
+  case when pickup_time = 'null' then null
+  	else pickup_time
+  	end as pickup_time,
+  case when distance = 'null' then null
+  	when distance like '%km' then trim('km' from distance)
+  	else distance
+  	end as distance,
+  case when duration = 'null' then null
+  	when duration like '%mins' then trim('mins' from duration)
+    when duration like '%minutes' then trim('minutes' from duration)
+  	when duration like '%minute' then trim('minute' from duration)
+  	else duration
+  	end as duration,
+  cancellation
+  from pizza_runner.runner_orders
+  ),
+  cte2 as(
+select order_id, runner_id, cast(pickup_time as timestamp), cast(distance as numeric),cast(duration as numeric) from cte
+)
+select max(duration)-min(duration) as difference from cte2
