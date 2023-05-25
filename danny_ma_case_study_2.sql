1a65f41f2d7add50b443196a7ab1cadbe8513d8b
Nandini13-rgb <65849848+Nandini13-rgb@users.noreply.github.com>
2023-05-25 19:28:00 +0530

    Update danny_ma_case_study_2.sql

--- danny_ma_case_study_2.sql
+++ danny_ma_case_study_2.sql
@@ -12,3 +12,42 @@ count(pizza_id) as total_volume
 from cte
 group by hour
 
+-- Question 10 Query:
+select
+to_char(order_time, 'day') as day,
+count(pizza_id) as total_pizza
+from
+pizza_runner.customer_orders
+group by 
+day
+order by total_pizza;
+
+-- Question 4 Query:
+with cte as(
+  select order_id,
+  runner_id,
+  case when pickup_time = 'null' then null
+  	else pickup_time
+  	end as pickup_time,
+  case when distance = 'null' then null
+  	when distance like '%km' then trim('km' from distance)
+  	else distance
+  	end as distance,
+  case when duration = 'null' then null
+  	when duration like '%mins' then trim('mins' from duration)
+    when duration like '%minutes' then trim('minutes' from duration)
+  	when duration like '%minute' then trim('minute' from duration)
+  	else duration
+  	end as duration,
+  cancellation
+  from pizza_runner.runner_orders
+  ),
+  cte2 as(
+select order_id, runner_id, cast(pickup_time as timestamp), cast(distance as numeric),duration from cte
+)
+select o.customer_id,round(avg(c.distance),1) as avg_distance from cte2 as c
+join pizza_runner.customer_orders as o on
+c.order_id = o.order_id
+group by o.customer_id
+;
+
