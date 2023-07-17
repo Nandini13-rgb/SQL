e8c3a773b9363038549929958be9a63ba048d7e1
Nandini13-rgb <65849848+Nandini13-rgb@users.noreply.github.com>
2023-07-17 22:53:55 +0530

    Update case_study_8.sql

--- case_study_8.sql
+++ case_study_8.sql
@@ -79,3 +79,82 @@ with cte as(
   select interest_id, count(month_year) from cte
   group by interest_id
   having count(month_year) = (select count(distinct month_year) from cte)
+-- If we were to remove all interest_id values which are lower than the total_months value we found in the previous question - how many total data points would we be removing?
+ temp as(
+  select interest_id, count(month_year) from cte
+  group by interest_id
+  having count(month_year) < (select count(distinct month_year) from cte))
+  select count(interest_id) from temp
+--After removing these interests - how many unique interests are there for each month?
+alter table fresh_segments.interest_metrics alter column month_year type varchar(15);
+update fresh_segments.interest_metrics set  month_year = to_date(month_year, 'MM-YYYY');
+alter table fresh_segments.interest_metrics alter column month_year type date using month_year :: date;
+with cte as(
+  SELECT cast(interest_id as integer) as interest_id,
+  _month,
+  _year,
+  month_year,
+  composition,
+  index_value,
+  ranking,
+  percentile_ranking
+  FROM
+  fresh_segments.interest_metrics 
+  ),
+   temp as(
+  select interest_id, count(month_year) from cte
+  group by interest_id
+  having count(month_year) < (select count(distinct month_year) from cte))
+  delete from fresh_segments.interest_metrics  where cast(interest_id as integer) in (select interest_id from temp);
+    select extract(month from month_year), count(distinct cast(interest_id as integer))
+    from fresh_segments.interest_metrics 
+    group by extract(month from month_year);
+
+--Using our filtered dataset by removing the interests with less than 6 months worth of data, which are the top 10 and bottom 10 interests which have the largest composition values in any month_year? Only use the maximum composition value for each interest
+ with max_composition as (select month_year, interest_id,
+    max(composition) as max_composition
+    from fresh_segments.interest_metrics 
+    group by interest_id, month_year)
+    (select * from max_composition
+    order by max_composition desc
+    limit 10)
+    union
+    (select  * from max_composition
+    order by max_composition asc
+    limit 10)
+
+-- Which 5 interests had the lowest average ranking value?  
+with avg_ranking as (select interest_id,
+    round(avg(ranking),2) as ranking_value
+    from fresh_segments.interest_metrics 
+    group by interest_id)
+    select * from avg_ranking
+    order by ranking_value 
+    limit 5
+    ;
+-- Which 5 interests had the largest standard deviation in their percentile_ranking value?  
+with standard_deviation_ranking as (select interest_id,
+   stddev(percentile_ranking) as ranking_value
+    from fresh_segments.interest_metrics 
+    group by interest_id)
+    select * from standard_deviation_ranking
+    order by ranking_value DESC
+    limit 5
+-- For the 5 interests found in the previous question - what was minimum and maximum percentile_ranking values for each interest and its corresponding year_month value? Can you describe what is happening for these 5 interests?
+with standard_deviation_ranking as (select interest_id,
+   stddev(percentile_ranking) as ranking_value
+    from fresh_segments.interest_metrics 
+    group by interest_id),
+    largest_stdev as(
+    select * from standard_deviation_ranking
+    order by ranking_value DESC
+    limit 5)
+    select s.interest_id,
+    max(m.percentile_ranking) as  max_percentile_ranking, min(m.percentile_ranking) as min_percentile_ranking,
+    s.ranking_value
+    from largest_stdev s
+    left join fresh_segments.interest_metrics m
+    on s.interest_id = m.interest_id
+    group by s.interest_id, s.ranking_value
+  
+    ;
